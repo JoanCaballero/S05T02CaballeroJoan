570d2b6a658f8c01cdafbbbc0244936d5be185bb
JoanCaballero <caballero.portoles.joan@gmail.com>
2024-04-04 12:34:58 +0200

    Tests

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t02/S05T02CaballeroJoan/Controllers/AuthenticationController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t02/S05T02CaballeroJoan/Controllers/AuthenticationController.java
@@ -4,7 +4,6 @@ import cat.itacademy.barcelonactiva.cognoms.nom.s05.t02.S05T02CaballeroJoan.Mode
 import cat.itacademy.barcelonactiva.cognoms.nom.s05.t02.S05T02CaballeroJoan.Model.DTO.SignInDTO;
 import cat.itacademy.barcelonactiva.cognoms.nom.s05.t02.S05T02CaballeroJoan.Model.DTO.SignUpDTO;
 import cat.itacademy.barcelonactiva.cognoms.nom.s05.t02.S05T02CaballeroJoan.Model.Service.Interfaces.AuthenticationService;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +19,11 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
-    @Operation(summary = "Register a new user")
     @PostMapping("/signup")
     public ResponseEntity<AuthenticationResponse> signUp(@RequestBody SignUpDTO signUp){
         return ResponseEntity.ok(authenticationService.signUp(signUp));
     }
 
-    @Operation(summary = "Sign in as an existing user")
     @PostMapping("/signin")
     public ResponseEntity<AuthenticationResponse> signIn(@RequestBody SignInDTO signIn) {
         return ResponseEntity.ok(authenticationService.signIn(signIn));
